f9a1a983a10f7edcca986dc348a97351bd02dae3
MaruDE100S <fafar9873@gmail.com>
2026-05-17 19:47:29 +0200

    if sctructure changed to switch

--- FNAZ.cpp
+++ FNAZ.cpp
@@ -16,16 +16,18 @@ void updateGame() {
 
 void drawGame() {
     BeginDrawing();
-    if (gameState == GameState::MENU) {
+    switch (gameState) {
+        case GameState::MENU:
         ClearBackground(BLACK);
         const char* title = "Five Nights at ZSI's";
         const char* subtitle = "Press Enter to start new game.";
-        const char* credit = "Developed by MaruDE\nSoundTrack and SFX by Mati FL Studio\n3D Modeling by Kreatynka";
+        const char* credit = "Developed by MaruDE\nSoundTrack and SFX by Mati FL Studio\n3D Modeling by Kreatynka\nVoice Acting by JohnyPendrive";
         const char* version = "0.0.0v";
         DrawText(title, 1280 / 2 - MeasureText(title, 50) / 2, 720 / 2 - 50, 50, WHITE);
         DrawText(subtitle, 1280 / 2 - MeasureText(subtitle, 20) / 2, 720 / 2 + 50, 20, WHITE);
-        DrawText(credit, 10, 720 - 80, 20, WHITE);
+        DrawText(credit, 10, 720 - 100, 20, WHITE);
         DrawText(version, 10, 10, 20, WHITE);
+        break;
     }
     EndDrawing();
 }
