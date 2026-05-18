72adeef7c6007ba77ba0f5bb9b5d615f86c59db6
MaruDE100S <fafar9873@gmail.com>
2026-05-18 15:28:48 +0200

    Fullscreen added

--- FNAZ.cpp
+++ FNAZ.cpp
@@ -26,7 +26,7 @@ struct GameContext {
 
 void loadAssets(gameAssets& assets) {
     // noise texture
-    Image noiseImage = GenImageWhiteNoise(1280, 720, 0.5f);
+    Image noiseImage = GenImageWhiteNoise(GetScreenWidth(), GetScreenHeight(), 0.5f);
     assets.noiseTexture = LoadTextureFromImage(noiseImage);
     UnloadImage(noiseImage);
     // music
@@ -41,16 +41,31 @@ void unloadAssets(gameAssets& assets) {
 
 void drawNoise(const gameAssets& assets) {
     float alpha = (float)GetRandomValue(5,15) / 100.0f;
-    DrawTextureEx(assets.noiseTexture, {(float)GetRandomValue(-5,5), 
-        (float)GetRandomValue(-5,5)}, 0, 1.0f, ColorAlpha(WHITE, alpha));
+    Rectangle source = {
+        0.0f,
+        0.0f,
+        (float)assets.noiseTexture.width,
+        (float)assets.noiseTexture.height
+    };
+    Rectangle dest = {
+        (float)GetRandomValue(-5,5),
+        (float)GetRandomValue(-5,5),
+        (float)GetScreenWidth() + 10,
+        (float)GetScreenHeight() + 10
+    };
+    DrawTexturePro(assets.noiseTexture, source, dest, {0,0}, 0.0f, ColorAlpha(WHITE, alpha));
     if (GetRandomValue(0, 100) > 95) {
-        DrawRectangle(0, GetRandomValue(0, 720), 1280, GetRandomValue(1, 4), 
+        DrawRectangle(0, GetRandomValue(0, GetScreenHeight()), GetScreenWidth(), GetRandomValue(1, 4), 
         ColorAlpha(BLACK, 0.3f));
     }
 }
 
 void updateGame(GameContext& context, gameAssets& assets) {
     UpdateMusicStream(assets.mainMenuMusic);
+
+    if (IsKeyPressed(KEY_F11)) {
+        ToggleFullscreen();
+    }
     
     if (context.state == GameState::MENU && IsKeyPressed(KEY_ENTER)) {
         // context.state = GameState::GAMEPLAY; 
@@ -64,12 +79,14 @@ void drawGame(const GameContext& context, const gameAssets& assets) {
         case GameState::MENU:
             ClearBackground(BLACK);
             const char* title = "Five Nights at ZSI's";
+            const char* fullscreen = "Press F11 to toggle fullscreen.";
             const char* subtitle = "Press Enter to start new game.";
             const char* credit = "Developed by MaruDE\nSoundTrack and SFX by Mati FL Studio\n3D Modeling by Kreatynka\nVoice Acting by JohnyPendrive";
             const char* version = "v0.0.0";
-            DrawText(title, 1280 / 2 - MeasureText(title, 50) / 2, 720 / 2 - 50, 50, WHITE);
-            DrawText(subtitle, 1280 / 2 - MeasureText(subtitle, 20) / 2, 720 / 2 + 50, 20, WHITE);
-            DrawText(credit, 10, 720 - 100, 20, WHITE);
+            DrawText(title, GetScreenWidth() / 2 - MeasureText(title, 50) / 2, GetScreenHeight() / 2 - 50, 50, WHITE);
+            DrawText(subtitle, GetScreenWidth() / 2 - MeasureText(subtitle, 20) / 2, GetScreenHeight() / 2 + 50, 20, WHITE);
+            DrawText(fullscreen, GetScreenWidth() - MeasureText(fullscreen, 20) - 10, 10, 20, WHITE);
+            DrawText(credit, 10, GetScreenHeight() - 100, 20, WHITE);
             DrawText(version, 10, 10, 20, WHITE);
             drawNoise(assets);
         break;
@@ -86,6 +103,7 @@ int main() {
     GameContext context;
     
     std::cout << "#LoveAngelika" << std::endl;
+    
     PlayMusicStream(assets.mainMenuMusic);
     while (!WindowShouldClose()) {
         updateGame(context, assets);
